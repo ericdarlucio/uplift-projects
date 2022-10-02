a254f63512d93c2f762d46138d0b0eadd30df6f1
John Eric Darlucio <ericdarlucio.ece@gmail.com>
2022-10-02 13:38:24 +0800

    feat: Add collision detection

--- p2-js-game/script.js
+++ p2-js-game/script.js
@@ -1,75 +1,78 @@
-// Create canvas element and attributes
-const canvas = document.createElement('canvas');
-canvas.width = '288';
-canvas.height = '512';
-canvas.id = 'cvs';
-canvas.style.border = '1px solid black'
+  // Get cvs
+  const cvs = document.getElementById('canvas');
 
-// Append canvas element to body
-document.body.appendChild(canvas);
+  // Get context
+  const ctx = cvs.getContext('2d');
 
-// Get context
-const ctx = cvs.getContext('2d');
+  // Load images
+  let bg = new Image();
+  let pipeNorth = new Image();
+  let pipeSouth = new Image();
+  let fg = new Image();
+  let bird = new Image();
 
-// Load images
-let bg = new Image();
-let pipeNorth = new Image();
-let pipeSouth = new Image();
-let fg = new Image();
-let bird = new Image();
+  bg.src = "image/bg.png";
+  pipeNorth.src = "image/pipeNorth.png";
+  pipeSouth.src = "image/pipeSouth.png";
+  fg.src = "image/fg.png";
+  bird.src = "image/bird.png";
 
-bg.src = "image/bg.png";
-pipeNorth.src = "image/pipeNorth.png";
-pipeSouth.src = "image/pipeSouth.png";
-fg.src = "image/fg.png";
-bird.src = "image/bird.png";
+  // Some variables
+  let gap = 120;
+  let constant = pipeNorth.height + gap;
+  let bX = 10;
+  let bY = 150;
+  let gravity = 1;
 
-// Some variables
-let gap = 85;
-let constant = pipeNorth.height + gap;
-let bX = 10;
-let bY = 150;
-let gravity = 1;
-
-// On keydown
-document.addEventListener('keydown', moveUp);
-function moveUp() {
-  bY -= 20;
-}
-
-// Pipe coordinates
-let pipe = [];
-pipe[0] = {
-  x : cvs.width,
-  y : 0
-}
-
-// Draw images
-function draw() {
-
-  ctx.drawImage(bg, 0, 0);
-
-  for (let i = 0; i < pipe.length; i++) {
-    ctx.drawImage(pipeNorth, pipe[i].x, pipe[i].y);
-    ctx.drawImage(pipeSouth, pipe[i].x, pipe[i].y + constant);  
-    pipe[i].x--;
-
-    if (pipe[i].x == 125) {
-      pipe.push({
-        x : cvs.width,
-        y : Math.floor(Math.random()*pipeNorth.height)-pipeNorth.height
-      });
-    }
-
+  // On keydown and on click
+  document.addEventListener('click', moveUp);
+  function moveUp() {
+    bY -= 20;
   }
 
-  ctx.drawImage(fg, 0, cvs.height - fg.height);
-  ctx.drawImage(bird, bX, bY);
-  bY += gravity;
-  requestAnimationFrame(draw);
-  
-}
+  document.addEventListener('keydown', moveUp);
+  function moveUp() {
+    bY -= 20;
+  }
 
-draw();
+  // Pipe coordinates
+  let pipe = [];
+  pipe[0] = {
+    x : cvs.width,
+    y : 0
+  }
+
+  // Draw images
+  function draw() {
+
+    ctx.drawImage(bg, 0, 0);
+
+    for (let i = 0; i < pipe.length; i++) {
+      ctx.drawImage(pipeNorth, pipe[i].x, pipe[i].y);
+      ctx.drawImage(pipeSouth, pipe[i].x, pipe[i].y + constant);  
+      pipe[i].x--;
+
+      if (pipe[i].x == 70) {
+        pipe.push({
+          x : cvs.width,
+          y : Math.floor(Math.random()*pipeNorth.height)-pipeNorth.height
+        });
+      }
+
+      // Detect collision
+      if ( bX + bird.width >= pipe[i].x && bX <= pipe[i].x + pipeNorth.width && ( bY <= pipe[i].y + pipeNorth.height || bY + bird.height >= pipe[i].y + constant) || bY + bird.height >= cvs.height - fg.height) {
+        location.reload(); 
+      }
+
+    }
+
+    ctx.drawImage(fg, 0, cvs.height - fg.height);
+    ctx.drawImage(bird, bX, bY);
+    bY += gravity;
 
 
+    requestAnimationFrame(draw);
+    
+  }
+
+  draw();
