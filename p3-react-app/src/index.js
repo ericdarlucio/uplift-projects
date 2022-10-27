f3ea32236b7db080410ae9e7185094db92f43ae2
John Eric Darlucio <ericdarlucio.ece@gmail.com>
2022-10-27 15:56:43 +0800

    feat: Setup redux

--- p3-react-app/src/index.js
+++ p3-react-app/src/index.js
@@ -7,11 +7,17 @@ import reportWebVitals from './reportWebVitals';
 // Routing Import
 import { BrowserRouter } from 'react-router-dom'
 
+// Redux Import
+import store from './redux/store';
+import { Provider } from 'react-redux';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
