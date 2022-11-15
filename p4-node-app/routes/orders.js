500185350ca21dd1824b040928f19bab871a6040
John Eric Darlucio <ericdarlucio.ece@gmail.com>
2022-11-15 15:26:05 +0800

    feat: Add delete and update order

--- p4-node-app/routes/orders.js
+++ p4-node-app/routes/orders.js
@@ -13,14 +13,30 @@ router.get('/', (request, response) => {
 router.post('/', (request, response) => {
   let newOrder = new Order(request.body);
   newOrder.save().then( result => {
-    Order.find().then( result => {
       response.send(result);
-    });
   });
 });
 
-router.put('/:id', (request, response) => {});
 
-router.delete('/', (request, response) => {});
+router.put('/:id', (request, response) => {
+  const orderId = request.params.id;
+  Order.updateOne(
+    { _id: orderId },
+    { $set: {...request.body} }
+  ).then(result => {
+    if(result.modifiedCount === 1){
+      response.send({ status: "Order has been updated" });
+    };
+  });
+});
+
+router.delete('/:id', (request, response) => {
+  const orderId = request.params.id;
+  Order.deleteOne({_id: orderId}).then( result => {
+    if( result.deletedCount === 1 ){
+        response.send({status: "Order has been deleted"});
+    }
+  });
+});
 
 module.exports = router;
