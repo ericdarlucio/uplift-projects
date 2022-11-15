cfc746e49e3cd8b35f50223e5037e25a30ed2056
John Eric Darlucio <ericdarlucio.ece@gmail.com>
2022-11-15 22:19:50 +0800

    feat: Add helmet

--- p4-node-app/index.js
+++ p4-node-app/index.js
@@ -5,11 +5,13 @@ const mongoose = require('mongoose');
 const morgan = require('morgan')
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const helmet = require('helmet');
 
 //Middlewares
 server.use(bodyParser.json());
 server.use(cors());
 server.use(morgan('dev'));
+server.use(helmet());
 
 server.get('/', (request, response) => {
   response.send('Welcome to express!');
