37a6c4bc96500a86d0a2a3afeb74ee99cc75aa98
John Eric Darlucio <ericdarlucio.ece@gmail.com>
2022-11-15 22:08:31 +0800

    feat: Add comments and change port

--- p4-node-app/index.js
+++ p4-node-app/index.js
@@ -1,23 +1,32 @@
 const express = require('express');
 const server = express();
+const port = 8030;
 const mongoose = require('mongoose'); 
 const morgan = require('morgan')
-const port = 8080;
-const OrderRouter = require('./routes/orders');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+
+//Middlewares
+server.use(bodyParser.json());
+server.use(cors());
+server.use(morgan('dev'));
 
 server.get('/', (request, response) => {
   response.send('Welcome to express!');
 });
 
-server.use(bodyParser.json());
-server.use(cors());
-server.use(morgan('dev'));
+// Router
+const OrderRouter = require('./routes/orders');
+const AuthRouter = require('./routes/auth');
 
+// Router Endpoints
 server.use('/api/v1/orders', OrderRouter);
+server.use('/api/v1/auth', AuthRouter);
 
+
+//MongoDB Connect
 mongoose.connect('mongodb://localhost:27017/water-deliverydb');
+
 
 server.listen(port, (request, response) => {
   console.log('Listening on port', port);
