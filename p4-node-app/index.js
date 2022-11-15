42d85642ae25bf024d348f07e9498e9645c0a846
John Eric Darlucio <ericdarlucio.ece@gmail.com>
2022-11-15 08:24:12 +0800

    feat: Add models and routes folder

--- p4-node-app/index.js
+++ p4-node-app/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const server = express();
 const port = 8080;
 
+server.get('/', (request, response) => {
+  response.send('Welcome to express!');
+});
+
 server.listen(port, (request, response) => {
   console.log('Listening on port', port);
-})
+});
