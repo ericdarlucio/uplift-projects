46362ed8837c0bfdce6ab8af542fc33df3a25200
John Eric Darlucio <ericdarlucio.ece@gmail.com>
2022-11-15 08:31:50 +0800

    feat: Add html requests at orders.js

--- p4-node-app/index.js
+++ p4-node-app/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const server = express();
 const port = 8080;
 
+const OrderRouter = require('./routes/orders');
+
+server.use('/api/v1/orders', OrderRouter);
+
 server.get('/', (request, response) => {
   response.send('Welcome to express!');
 });
---
+++ p4-node-app/routes/orders.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (request, response) => {})
+router.post('/', (request, response) => {})
+router.put('/', (request, response) => {})
+router.delete('/', (request, response) => {})
+
+module.exports = router;
