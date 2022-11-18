f30dafb02bf4fd3ecde786a5192d0ff304fd6dbb
John Eric Darlucio <ericdarlucio.ece@gmail.com>
2022-11-19 00:26:43 +0800

    feat: Add backend folder, models and business schema

---
+++ p5-mern-app/backend/models/Business.js
@@ -0,0 +1,26 @@
+const mongoose = require(mongoose);
+
+const BusinessSchema = new mongoose.Schema({
+  
+  email: String,
+  password: String,
+  businessName: String,
+  businessCategory: String,
+  businessStatus: String,
+  aboutUs: String,
+  address: {
+    streetNumber: String,
+    streetName: String,
+    barangay: String,
+    longiture: String,
+    latitude: String
+  },
+  contactNumber: String,
+  photos: [],
+  reviews: [
+    { type: mongoose.Types.ObjectId, ref: 'Ref'}
+  ]
+
+});
+
+module.exports = mongoose.model('Business', BusinessSchema);
