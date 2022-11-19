04863f77295a49f3e7afdedc4adbd0133564a5a4
John Eric Darlucio <ericdarlucio.ece@gmail.com>
2022-11-19 15:00:01 +0800

    feat: Add routes and middlewares

--- p5-mern-app/backend/index.js
+++ p5-mern-app/backend/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const server = express();
 const port = 8080;
+
+const mongoose = require('mongoose');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const helmet = require('helmet');
 
 server.use(morgan('dev'));
+server.use(bodyParser.json());
+server.use(cors());
+server.use(helmet());
 
 server.get('/', (request, response) => {
   response.send('Welcome to ExpressJS!');
---
+++ p5-mern-app/backend/routes/businesses.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express();
+
+const Business = require('../models/Business');
+
+router.get('/', (request, ))
+
+module.exports = router;
