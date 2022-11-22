cc4f26df4784a059ebb0cc7e78b156acdf536a31
John Eric Darlucio <ericdarlucio.ece@gmail.com>
2022-11-23 01:38:52 +0800

    feat: Add link to pages

--- p5-mern-app/frontend/src/pages/Hero.js
+++ p5-mern-app/frontend/src/pages/Hero.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import './Hero.css';
@@ -12,12 +13,12 @@ const Hero = () => {
         <div className="Hero-content">
           <h1>Helping local businesses attract more customers</h1>
           <p>Make it easy for the customers to find you</p>
-          <button>Register your business now!</button>
+          <Link to='/register'><button>Register your business now!</button></Link>
         </div>
         <div className="Hero-subcontent">
           <h2>Discover local businesses nearby</h2>
           <p>Local businesses need our support</p>
-          <button>See the businesses</button>
+          <Link to='/business-list'><button>See all businesses</button></Link>
         </div>
       </div>
 
