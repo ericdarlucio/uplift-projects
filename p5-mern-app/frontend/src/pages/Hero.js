657345a33b48ed1e67114669ca6ccf2d867abc67
John Eric Darlucio <ericdarlucio.ece@gmail.com>
2022-11-22 23:34:41 +0800

    feat: Add css to hero page

--- p5-mern-app/frontend/src/pages/Hero.js
+++ p5-mern-app/frontend/src/pages/Hero.js
@@ -1,10 +1,28 @@
+import Footer from "../components/Footer";
 import Header from "../components/Header";
+import './Hero.css';
 
 const Hero = () => {
   return (
-    <div>
+    <div className="Hero-background">
+
       <Header/>
-      Homepage
+
+      <div className="Hero-container">
+        <div className="Hero-content">
+          <h1>Helping local businesses attract more customers</h1>
+          <p>Make it easy for the customers to find you</p>
+          <button>Register your business now!</button>
+        </div>
+        <div className="Hero-subcontent">
+          <h2>Discover local businesses nearby</h2>
+          <p>Local businesses need our support</p>
+          <button>See the businesses</button>
+        </div>
+      </div>
+
+      <Footer/>
+
     </div>
   )
 };
