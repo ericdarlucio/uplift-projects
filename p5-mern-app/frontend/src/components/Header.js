2227faafae15f9867f28640c6d52a3e390b65fe3
John Eric Darlucio <ericdarlucio.ece@gmail.com>
2022-11-23 01:38:02 +0800

    fix: Modify css and add navigation

--- p5-mern-app/frontend/src/components/Header.js
+++ p5-mern-app/frontend/src/components/Header.js
@@ -1,13 +1,32 @@
 import './Header.css';
-import {Link} from 'react-router-dom';
+import {Link, useNavigate} from 'react-router-dom';
 
 const Header = () => {
+
+  const clientInStorage = localStorage.getItem('userId');
+  const navigate = useNavigate();
+
   return (
     <div className='Header-container'>
-      <Link to='/'>Homepage</Link>
+      <Link to='/'><img className='Header-image' src={require('../images/home.png')} alt='login cartoon'></img></Link>
       <div className='Header-right'>
-        <Link to='/register'>Register</Link>
-        <Link to='/login'>Login</Link>
+        { clientInStorage === null ?
+          <>
+            <Link to='/register'><button className='Header-register'>Register</button></Link>
+            <Link to='/login'><button className='Header-login'>Login</button></Link>
+          </> :
+          <>
+            <button 
+              className='Header-login'
+              onClick={() => {
+                window.localStorage.clear();
+                navigate('/login');
+              }}
+            >Logout</button>
+          </>
+
+        }
+        
       </div>
     </div>
   )
